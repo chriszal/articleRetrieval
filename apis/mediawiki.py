38ecd15b8ad7a4d2af68b046c6d5d790bcf24cdf
chriszal <christoszal@gmail.com>
2022-12-08 14:12:13 +0200

    api fixes

--- apis/mediawiki.py
+++ apis/mediawiki.py
@@ -5,5 +5,10 @@ class MediaWikiApi():
     def __init__(self):
         self.mediawiki = MediaWiki()
 
-    def get_page_from_domain(self,domain_name):
-        return self.mediawiki.search(domain_name)
+    def get_source_domain_info(self,sources):
+        source_info={}
+        for source in sources:
+            article = self.mediawiki.search(source)
+            if article:
+                source_info[source]=article['description']
+        return source_info
--- apis/newsapi.py
+++ apis/newsapi.py
@@ -1,23 +1,16 @@
 from newsapi import NewsApiClient
+import requests
 
 # Init
 class NewsApi():
     def __init__(self):
-        self.newsapi = NewsApiClient(api_key='420d1582b00240789d9fc79e7943e61f')
-        self.topics=["motorsport","agriculture","war"]
+        self.secret = "2a2624e83ecb4b7f9a3e8bc1798d6946"
 
-    def get_articles(self):
-        for topic in self.topics:
-            top_headlines = self.newsapi.get_top_headlines(q=topic)
-            Headlines = top_headlines['articles']
-            
-            # now we will display the that news with a good readability for user
-            if Headlines:
-                    for articles in Headlines:
-                        b = articles['title'][::-1].index("-")
-                        if "news" in (articles['title'][-b+1:]).lower():
-                            print(str(articles['title'][-b+1:])+":"+ str(articles['title'][:-b-2]))
-                        else:
-                            print(str(articles['title'][-b+1:])+" News: "+str(articles['title'][:-b-2]))
-            else:
-                print("Sorry no articles found for "+str(topic)+", Something Wrong!!!")
+    def get_articles(self,keyword):
+        response = requests.get("https://newsapi.org/v2/everything",params={'q':keyword,'apiKey': self.secret })
+        articles=[]
+        for article in response['articles']:
+            source = article['source']['name']
+            articles.append({'source':source,'article':article})
+        
+        return articles
