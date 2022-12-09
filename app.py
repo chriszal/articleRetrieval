e735cfec2e61463d833d6ec10433b1aec7662e40
Athanasioschourlias <thanos.chourlias@gmail.com>
2022-12-09 09:18:52 +0200

    changes

--- app.py
+++ app.py
@@ -1,42 +1,48 @@
 from flask import Flask, jsonify, request
-from assets.users import User
-from apis.newsapi import NewsApi
-from kafka_bus.producer import Producer
 from assets.database import Database
+import jellyfish
 import time
-from assets.ArticleSDDescriptions import SourceDomainDescriptions
-# import jellyfish
+import datetime
+# Mongo imports
+from pymongo import MongoClient
+from assets.models import Users
+# Flask imports
+from flask import Flask, jsonify, request
 
 # Name of the application module or package so flask knows where to look for resources
 app = Flask(__name__)
 
-TOPICS= ["agricuture",
-        "bussines",
-        "elon musk",
-        "motosport",
-        "science",
-        "space",
-        "technology",
-        "war"]
-# controllers implementations
-user = User()
-producer = Producer()
 db = Database()
-sourceDomainDesc = SourceDomainDescriptions()
+
 
 @app.route('/')
 def index():
+    user = {
+        "keywords": ["test", "test2"],
+        "email": "test@gmail.com",
+        "created": datetime.datetime,
+        "city": "Testssssasdbasud"
+    }
+
+    user = Users(**user)
+
+    insert_result = db.users.insert_one(user.to_bson())
+
+    print(insert_result)
+    print(user.to_bson())
+
     return jsonify(
         status=True,
         message='Welcome !'
     )
+
 
 # @app.get("/keyword/articles/<string:keyword>")
 # def fetch_users_articles_controller(user_keyword):
 #     """
 #     :param keyword:
 #     :return: The articles that corespond to this keyword
-
+#
 #     :Note: The available keywords the user can enter here are
 #       [
 #         agricuture,
@@ -58,14 +64,14 @@ def index():
 #         "technology",
 #         "war"
 #     ]
-
+#
 #     for keyword in keywords:
-
+#
 #         #Cosine similarity of the true title and the candidate
 #         if jellyfish.jaro_distance(keyword, user_keyword) >= 0.85:
 #             response = db.find_articles(user_keyword)
 #             return response, 201
-
+#
 #     return "There are no anvailable records for the given keyword, please use one that is supported and try again", 500
 
 # @app.get("/source/description/<string:sourcedomainname>")
@@ -74,29 +80,29 @@ def index():
 #     #Cosine similarity of the true title and the candidate
 #     response = sourceDomainDesc.fetch_domain_descriptionzç(sourcedomainname)
 #     return response
-
-
+#
+#
 # @app.get("/user/articles/<int:user_id>")
 # def fetch_users_articles_controller():
 #     return "<p>Return users articles bases on id of fail if invalid user id is provided</p>"
-
-@app.post("/create/user")
-def create_user_controller():
-    data = request.get_json()
-    response = user.create(data)
-
-    return response, 201
-
-@app.put("/edit/user/keywords/<string:email>")
-def add_new_user_controller(email):
-    data = request.get_json()
-    response = user.update(email, data)
-    return response, 201
-
-@app.delete("/delete/user/<string:email>")
-def delete_user_controller(email):
-    count = user.delete(email)
-    return "Deleted entities: " + str(count)
+#
+# @app.post("/create/user")
+# def create_user_controller():
+#     data = request.get_json()
+#     response = user.create(data)
+#
+#     return response, 201
+#
+# @app.put("/edit/user/keywords/<string:email>")
+# def add_new_user_controller(email):
+#     data = request.get_json()
+#     response = user.update(email, data)
+#     return response, 201
+#
+# @app.delete("/delete/user/<string:email>")
+# def delete_user_controller(email):
+#     count = user.delete(email)
+#     return "Deleted entities: " + str(count)
 
 if __name__ == "__main__":
     # Creating a new connection with mongo
