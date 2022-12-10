fdbeb633420e1379499099c6deed3d5b2b651ea3
chriszal <christoszal@gmail.com>
2022-12-11 00:24:48 +0200

    fixed kafka

--- kafka_bus/kafkaProducerThread.py
+++ kafka_bus/kafkaProducerThread.py
@@ -1,8 +1,10 @@
 from kafka import KafkaProducer
 from threading import Thread
 import time
+import json
 from apis.newsapi import NewsApi
 from apis.mediawiki import MediaWikiApi
+
 
 class KafkaProducerThread(Thread):
     def __init__(self,TOPICS):
@@ -15,19 +17,20 @@ class KafkaProducerThread(Thread):
     def run(self):
         # Initialize the Kafka producer
         producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
-            max_block_ms=100000,)
+            max_block_ms=100000,
+            value_serializer=lambda x: json.dumps(x).encode('utf-8'))
 
         # Function to call the News API and publish the retrieved articles to the corresponding Kafka topic
         def call_apis():
             # Iterate over the keywords
             domains=[]
-            for topic in self.TOPICS:
+            for topic in self.topics:
                 articles = self.news_api.get_articles(topic)
                 for article in articles:
-                    if article['source'] is not '':
+                    if article['source'] != '':
                         if article['source'] not in domains:
                             domains.append(article['source'])
-                        producer.send(topic,article)
+                        producer.send(topic,value=article)
             
             for domain in domains:
                 source_info = self.media_api.get_source_domain_info(domain)
--- kafka_bus/testC.py
+++ kafka_bus/testC.py
@@ -1,34 +1,49 @@
 from kafka import KafkaConsumer
 from threading import Thread
 import json
-from assets.database import Database
+# from assets.database import Database
 
 class KafkaConsumerThread(Thread):
     def __init__(self,TOPICS):
         Thread.__init__(self)
         self.topics = TOPICS
-        self.db = Database()
+        # self.db = Database()
 
     def run(self):
         print("Start Consumer Thread")
         consumer = KafkaConsumer(bootstrap_servers=['localhost:9092'],
-                auto_offset_reset='earliest',
-                enable_auto_commit=True,value_deserializer=lambda x: json.loads(x.decode('utf-8')))
+            auto_offset_reset='earliest',
+            enable_auto_commit=True,value_deserializer=lambda x: json.loads(x.decode('utf-8')))
+
         for topic in self.topics:
 
+           
             consumer.subscribe(topic)
 
             # Iterate over the messages in the topic
             for message in consumer:
-                print(message)
+                if message.topic ==topic:
+                    print(message.value)
                # Save articles in corresponding MongoDB collection
-                self.db.keywords.insert_one(message.value)
+                # self.db.keywords.insert_one(message.value)
 
         # Initialize the Kafka consumer for the sources topic
         source_consumer = KafkaConsumer('sources', bootstrap_servers=['localhost:9092'], auto_offset_reset='earliest',value_deserializer=lambda x: json.loads(x.decode('utf-8')))
 
         for message in source_consumer:
-            print(message)
+            if message.topic =="sources":
+                print(message.value)
             # Save source domain name information in "sources" collection
-            self.db.ArticlesSDDescription.insert_one(message.value)
+            # self.db.ArticlesSDDescription.insert_one(message.value)
 
+if __name__ == "__main__":
+    TOPICS= ["education",
+            "health",
+            "business",
+            "motorsport",
+            "science",
+            "space",
+            "technology",
+            "war"]
+    consumer_thread = KafkaConsumerThread(TOPICS)
+    consumer_thread.start()
