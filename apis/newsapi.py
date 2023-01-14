dec3776d6b025247e0820e66160cebe3f9c33c6d
liapisn <nikos@liapis.info>
2023-01-14 12:09:43 +0200

    (chore) Code formatting

--- apis/newsapi.py
+++ apis/newsapi.py
@@ -1,26 +1,28 @@
 import requests
 
+
 # Init
 class NewsApi():
     def __init__(self):
         self.secret = "420d1582b00240789d9fc79e7943e61f"
 
-    def get_articles(self,keyword):
-        response = requests.get("https://newsapi.org/v2/everything",params={'q':keyword,'apiKey': self.secret ,'language':'en'})
+    def get_articles(self, keyword):
+        response = requests.get("https://newsapi.org/v2/everything",
+                                params={'q': keyword, 'apiKey': self.secret, 'language': 'en'})
         if response.status_code == 200:
             response_dict = response.json()
-            articles=[]
-        
+            articles = []
+
             for article in response_dict['articles']:
                 source = article['source']['name']
-                articles.append({'source':source,'article':article['content']})
-        
+                articles.append({'source': source, 'article': article['content']})
+
             if not articles:
                 articles = [{'source': '', 'article': ''}]
             return articles
         else:
             return [{'source': '', 'article': ''}]
-        
+
     #     articles =[]
     # for keyword in keywords:
     #     articles.append(news.get_articles(keyword))
--- kafka_bus/kafkaConsumerThread.py
+++ kafka_bus/kafkaConsumerThread.py
@@ -17,23 +17,23 @@ class KafkaConsumerThread:
         # self.logger.debug("Initializing the consumer")
 
     def start(self):
-         # Wait for a few seconds before starting the Kafka consumer
+        # Wait for a few seconds before starting the Kafka consumer
         # time.sleep(15)
         # self.logger.debug("Getting the kafka consumer")
         try:
             consumer = KafkaConsumer(bootstrap_servers=['kafka:29092'],
-                            auto_offset_reset='earliest',
-                            enable_auto_commit=True,
-                            value_deserializer=lambda x: json.loads(x.decode('utf-8')))
+                                     auto_offset_reset='earliest',
+                                     enable_auto_commit=True,
+                                     value_deserializer=lambda x: json.loads(x.decode('utf-8')))
         except NoBrokersAvailable as err:
             # self.logger.error("Unable to find a broker: {0}".format(err))
             time.sleep(1)
-        consumer.subscribe(self.topics+["sources"])
-        
+        consumer.subscribe(self.topics + ["sources"])
+
         # try:
         # if consumer:
         for message in consumer:
-            if message.topic=="sources":
+            if message.topic == "sources":
                 self.db.insert_source_info(message.value["source_name"], message.value["source_info"])
             else:
                 self.db.insert_article(message.topic, [message.value])
@@ -43,4 +43,3 @@ class KafkaConsumerThread:
         #                       "There is no consumer to read from.")
         #     self.logger.error(str(ae))
         #     raise ConnectionException
-
