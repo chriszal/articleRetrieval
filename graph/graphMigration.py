2be60a90255520f8ed9727a9f8702e5589927a80
chriszal <christoszal@gmail.com>
2023-01-14 14:55:34 +0200

    chourlia check

--- graph/graphMigration.py
+++ graph/graphMigration.py
@@ -2,8 +2,13 @@ import networkx as nx
 import json
 
 class GraphMigration:
-    def __init__(self, db):
+    def __init__(self,topics, db):
         self.db = db
+        self.topics = topics
         self.G = nx.Graph()
 
-    
+    def create_nodes(self):
+        for topic in self.topics:
+            cursor_article = self.db[f"{topic}"].find()
+            for article in cursor_article:
+                self.G.add_node(article["_id"], source=article["source"], author=article["author"], timestamp=article["timestamp"])
